77f32cc5785b0fff3a361e58f0fc0c26dae59ae5
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2025-08-25 11:40:22 +0530

    Update document head with favicon and remove unused favicon file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/Logo-fav-icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
